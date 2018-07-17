c2528164eec8cfd0c27fc12cadac4f457379a73a
Fabricio Oliveira <frolivei@gmail.com>
2018-07-17 17:38:18 -0300

    Add new route get.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('teste');
+    res.send('<h1>It\'s running...</h1>')
 });
+
 app.listen(3000);
